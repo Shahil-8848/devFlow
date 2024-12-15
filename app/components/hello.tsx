65dfe4e26d6f8e1e84f045c6289f9cd6ac572885
Shahil Chettri <shahil001785@gmail.com>
2024-12-15 20:19:10 +0545

    Create a nextjs app

---
+++ app/components/hello.tsx
@@ -0,0 +1,10 @@
+"use client";
+function Hello() {
+  console.log("I am a client component");
+  return (
+    <div>
+      <h1>Hello Shahil</h1>
+    </div>
+  );
+}
+export default Hello;
---
+++ app/dashboard/main/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>this is a main page</div>;
+};
+
+export default page;
