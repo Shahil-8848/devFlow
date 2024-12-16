2e64f7fda09e55dd28e5a78c2dc9e984a95beb04
Shahil Chettri <shahil001785@gmail.com>
2024-12-16 13:38:58 +0545

    assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import Hello from "./components/hello";
 
 export default function Home() {
   console.log("Its the response simple of course");
   return (
     <>
-      <h1 className="text-3xl text-brown">Ready for your next journey</h1>
+      <h1 className="text-3xl">Ready for your next journey</h1>
       <Hello />
     </>
   );
