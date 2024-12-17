6a8f4719e95eef3696af7ac64e517247198aefbc
Shahil Chettri <shahil001785@gmail.com>
2024-12-17 13:51:50 +0545

    setup Light and dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
 import Hello from "./components/hello";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export default function Home() {
   console.log("Its the response simple of course");
   return (
     <>
-      <h1 className="text-3xl">Ready for your next journey</h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
       <Hello />
     </>
   );
