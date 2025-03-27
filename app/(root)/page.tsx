8bfad7e0e0b375402f93e70d3155b82b55013832
Shahil Chettri <shahil001785@gmail.com>
2025-03-27 21:14:42 +0545

    My works

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,26 @@
-import { auth,  } from "@/auth";
-
+import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
 const Home = async () => {
   const session = await auth();
 
-console.log(session)
+  console.log(session);
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
-      
+      <section className="flex w-full flex-col-reverse justify-between gap-4">
+        <h1 className="h1-bold">Project Questions</h1>
+        <Button>
+          <Link href={ROUTES.ASK_QUESTION}>Ask a question</Link>
+        </Button>
+      </section>
+      <section>Local search</section>
+      <section>Local search</section>
+      <div>
+        <p>Question - 3</p>
+        <p>Question - 2</p>
+        <p>Question - 1</p>
+      </div>
     </>
   );
 };
